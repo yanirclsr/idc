62a4422c7d6c1b4ba6988ef00e1d0d329c3c4d67
Yanir Calisar <ycalisar@gmail.com>
2018-06-08 09:13:13 +0300

    minor

--- aws/src/main/java/Main.java
+++ aws/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import org.springframework.boot.SpringApplication;
 
 public class Main {
@@ -5,5 +7,11 @@ public class Main {
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
         System.out.println("ssa");
+        AWSCredentials credentials;
+        try {
+            credentials = new BasicAWSCredentials("XXXXXXXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        } catch (Exception e){
+
+        }
     }
 }
