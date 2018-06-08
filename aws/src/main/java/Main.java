1dd8767ea56831b342e6c685a25a2929fd457299
Yanir Calisar <ycalisar@gmail.com>
2018-06-08 09:09:30 +0300

    added spring boot added aws rekognition

--- aws/src/main/java/Main.java
+++ aws/src/main/java/Main.java
@@ -1,2 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
 public class Main {
+
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+        System.out.println("ssa");
+    }
 }
